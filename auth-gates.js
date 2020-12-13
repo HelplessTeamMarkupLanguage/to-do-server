6677700a6a235c5d67d4900e5199bef27ad125a1
Fodor Ádám <Fodor.Adam.1@stud.u-szege.hu>
2020-12-13 21:32:49 +0100

    fix bearer issue in auth-gates

--- auth-gates.js
+++ auth-gates.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[2];
+    const token = req.headers.authorization.split(' ')[1];
     const userId = jwt.verify(token, process.env.JWT_SECRET)._id;
     if (userId) {
       req.userId = userId;
